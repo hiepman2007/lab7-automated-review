0ca4440cbd220e1cfbaa5765009a1b055330d184
hiepman2007 <hiepman2007@gmail.com>
2026-05-06 21:55:27 +0700

    Create intentional errors

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int x=10+5; // Lỗi: Thiếu khoảng trắng quanh dấu = và +
+    int x=10+5; // Lỗi: Thiếu khoảng trắng quanh dấu = và + dsad asd a dá d
     System.out.println("Test bot");
   }
 }
