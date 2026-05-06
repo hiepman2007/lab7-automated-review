06ed1cc1cba1a2f35bd867f4d098d7e37bd426e8
hiepman2007 <hiepman2007@gmail.com>
2026-05-06 22:00:13 +0700

    Re-testing automated review

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
     int x=10+5; // Không có khoảng trắng nào
-    System.out.println("Re-testing bot");
+    System.out.println("Re-testinsdasdsadsag bot");
   }
 }
